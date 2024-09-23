016080d6f08357b804b5f9f239fb185c4585c3ff
Johann284 <johann_ruth@estudante.sesisenai.org.br>
2024-09-23 11:32:59 -0300

    criação de aulas base funcionando

--- att10_crud/add_aula.php
+++ att10_crud/add_aula.php
@@ -1,4 +1,55 @@
 <?php
 // Deve ser possível inserir uma nova aula no banco de dados.
-//comentário teste
+
+include"bd.php";
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $sala_aula = $_POST['sala'];
+        $dia = $_POST['dia'];
+        if($sala_aula == null || $dia == null){
+            echo 'Valor inválido';
+        } else {
+            $sql = "INSERT INTO aulas (sala_aula,dia_aula) VALUE ('$sala_aula', '$dia')";
+            if ($conn -> query($sql) === TRUE){
+                echo "Novo resgistro criado com sucesso!";
+            }else{
+                echo "Erro" . $sql . "<br>" . $conn -> error;
+            }
+        }
+        
+
+
+        
+    }
+ $conn -> close();
 ?>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create</title>
+</head>
+<body>
+    
+    <form method="POST" action="">
+        <label for="sala">Sala da Aula: </label>
+        <input type="text" name="sala" require>
+        <label for="dia">Dia da aula: </label>
+
+        <select name="dia" require>
+          <option value="segundaFeira">Segunda-Feira:</option>
+          <option value="tercaFeira">Terça-Feira</option>
+          <option value="quartaFeira">Quarta-Feira</option>
+          <option value="quintaFeira">Quinta-Feira</option>
+          <option value="sextaFeira">Sexta-Feira</option>
+        </select>
+        <br>
+        <input type="submit" value="Adicionar">
+    </form>
+
+    <a href="index.php"><button>Ver Quadro de horário.</button></a>
+    <a href="add_professor.php"><button>Inserir Professor.</button></a>
+
+
+</body>
+</html>
