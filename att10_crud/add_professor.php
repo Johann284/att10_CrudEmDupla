4efa98769ed994b73b3cee392d65215a07f918dc
Pavinis <160753747+Pavinis@users.noreply.github.com>
2024-09-23 12:49:14 -0300

    Update add_professor.php

--- att10_crud/add_professor.php
+++ att10_crud/add_professor.php
@@ -4,8 +4,10 @@ include 'bd.php';
 
 if ($_SERVER ['REQUEST_METHOD'] == 'POST') {
     $nome = $_POST['nome_professor'];
+    $formacao = $_POST['formacao'];
+    $cpf = $_POST['cpf'];
 
-    $sql = "INSERT INTO professores (nome_professor) VALUES ('$nome')";
+    $sql = "INSERT INTO professores (nome_professor, formacao, cpf) VALUES ('$nome', '$formacao', '$cpf')";
 
     if ($conn->query($sql) === TRUE) {
         echo "Novo professor registrado com sucesso";
@@ -18,6 +20,8 @@ $conn->close();
 
 <form method="POST" action="add_professor.php">
     Nome: <input type="text" name="nome_professor" required>
+    Formação: <input type="text" name="formacao" required>
+    CPF: <input type="text" name="cpf" required>
     <input type="submit" value="adicionar">
 </form>
 
